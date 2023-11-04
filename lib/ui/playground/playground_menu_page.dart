0c48700f68fea57eb10653fd7882d9058dd0d24c
khoruma <khoruma980as@gmail.com>
2023-11-04 21:34:56 +0700

    Modal Sheet Switch App

--- lib/ui/playground/playground_menu_page.dart
+++ lib/ui/playground/playground_menu_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bootcamp/core/common/assets_path.dart';
+import 'package:flutter_bootcamp/core/common/colors_const.dart';
 import 'package:flutter_bootcamp/core/utils/text_theme_extension.dart';
 import 'package:flutter_bootcamp/core/utils/ui_helper.dart';
 import 'package:flutter_bootcamp/ui/widgets/choose_option.dart';
+import 'package:flutter_svg/svg.dart';
 
 class PlaygroundMenuPage extends StatelessWidget {
   const PlaygroundMenuPage({super.key});
@@ -69,7 +72,93 @@ class PlaygroundMenuPage extends StatelessWidget {
                   title: 'Switch App',
                   subTitle:
                       'Goes to main home page and choose between playground or Pixels ',
-                  onTap: () {},
+                  onTap: () {
+                    showModalBottomSheet(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        builder: (context) {
+                          return Padding(
+                            padding: UIHelper.padding(
+                                top: 50, horizontal: UIHelper.defaultPadding),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Choose your app',
+                                  style: context.textTheme.headlineMedium,
+                                ),
+                                UIHelper.verticalSpace(20),
+                                Container(
+                                  padding: UIHelper.padding(left: 20),
+                                  width: double.infinity,
+                                  height: 70,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: ColorConstant.grey,
+                                      width: 0.5,
+                                    ),
+                                    borderRadius:
+                                        UIHelper.borderRadiusCircular(all: 10),
+                                  ),
+                                  child: SvgPicture.asset(
+                                    AssetsPath.pixelNewsLogo,
+                                    fit: BoxFit.scaleDown,
+                                    alignment: Alignment.centerLeft,
+                                  ),
+                                ),
+                                UIHelper.verticalSpace(20),
+                                Container(
+                                  padding: UIHelper.padding(left: 20),
+                                  width: double.infinity,
+                                  height: 70,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: ColorConstant.grey,
+                                      width: 0.5,
+                                    ),
+                                    borderRadius:
+                                        UIHelper.borderRadiusCircular(all: 10),
+                                  ),
+                                  child: Image.asset(
+                                    AssetsPath.playgroundLogo,
+                                    fit: BoxFit.scaleDown,
+                                    alignment: Alignment.centerLeft,
+                                  ),
+                                ),
+                                UIHelper.verticalSpace(20),
+                                Container(
+                                  padding: UIHelper.padding(left: 20),
+                                  width: double.infinity,
+                                  height: 70,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: ColorConstant.grey,
+                                      width: 0.5,
+                                    ),
+                                    borderRadius:
+                                        UIHelper.borderRadiusCircular(all: 10),
+                                  ),
+                                  child: Row(
+                                    children: [
+                                      SvgPicture.asset(
+                                        AssetsPath.restartIcon,
+                                        fit: BoxFit.scaleDown,
+                                        alignment: Alignment.centerLeft,
+                                      ),
+                                      Text(
+                                        'Reset App',
+                                        style: context.textTheme.bodyMedium,
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                        context: context);
+                  },
                 ),
               ],
             ),
