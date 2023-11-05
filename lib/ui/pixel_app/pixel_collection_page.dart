bb1b03bfb56ff918134f7902d199ed0ad21fcaf9
khoruma <khoruma980as@gmail.com>
2023-11-05 16:51:46 +0700

    Pixel Collection Page

--- lib/ui/pixel_app/pixel_collection_page.dart
+++ lib/ui/pixel_app/pixel_collection_page.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bootcamp/core/utils/text_theme_extension.dart';
+import 'package:flutter_bootcamp/core/utils/ui_helper.dart';
+import 'package:flutter_bootcamp/ui/widgets/dummy_card.dart';
 
 class PixelCollectionPage extends StatelessWidget {
   const PixelCollectionPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('collection page '),
+    return Scaffold(
+      body: Padding(
+        padding: UIHelper.padding(top: 50, horizontal: UIHelper.paddingContent),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Favorites',
+              style: context.textTheme.displayMedium,
+            ),
+            UIHelper.verticalSpace(20),
+            const DummyCard(),
+            const DummyCard(),
+            const DummyCard(),
+            const DummyCard(),
+          ],
+        ),
+      ),
     );
   }
 }
